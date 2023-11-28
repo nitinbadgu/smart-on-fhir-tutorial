e2aebbb751e6e335b3fb2abf35707c9dfffb1e8f
nitinbadgu <89129373+nitinbadgu@users.noreply.github.com>
2023-11-28 16:32:06 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,166 +1,166 @@
 /* its working for smart launcher start*/
-(function(window){
-  window.extractData = function() {
-    var ret = $.Deferred();
+// (function(window){
+//   window.extractData = function() {
+//     var ret = $.Deferred();
 
-    function onError() {
-      console.log('Loading error', arguments);
-      ret.reject();
-    }
+//     function onError() {
+//       console.log('Loading error', arguments);
+//       ret.reject();
+//     }
 
-    function onReady(smart)  {
-      if (smart.hasOwnProperty('patient')) {
-        var patient = smart.patient;
-        var pt = patient.read();
+//     function onReady(smart)  {
+//       if (smart.hasOwnProperty('patient')) {
+//         var patient = smart.patient;
+//         var pt = patient.read();
 
-        pt.done(function (data) { sessionStorage.setItem('patientDetails', JSON.stringify(data)); });
+//         pt.done(function (data) { sessionStorage.setItem('patientDetails', JSON.stringify(data)); });
         
-        var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
-                    }
-                  });
-      obv.done(function (data) { sessionStorage.setItem('observationDetails', JSON.stringify(data)); });
+//         var obv = smart.patient.api.fetchAll({
+//                     type: 'Observation',
+//                     query: {
+//                       code: {
+//                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+//                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+//                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+//                       }
+//                     }
+//                   });
+//       obv.done(function (data) { sessionStorage.setItem('observationDetails', JSON.stringify(data)); });
 
-      var cov = smart.patient.api.fetchAll({
-        type: "Coverage",
-      });
+//       var cov = smart.patient.api.fetchAll({
+//         type: "Coverage",
+//       });
 
-      cov.done(function (data) { sessionStorage.setItem('coverageDetails', JSON.stringify(data)); });
+//       cov.done(function (data) { sessionStorage.setItem('coverageDetails', JSON.stringify(data)); });
 
-      var org = smart.patient.api.fetchAll({
-        type: "Organization",
-      });
+//       var org = smart.patient.api.fetchAll({
+//         type: "Organization",
+//       });
 
-      org.done(function (data) { sessionStorage.setItem('orgDetails', JSON.stringify(data)); });
+//       org.done(function (data) { sessionStorage.setItem('orgDetails', JSON.stringify(data)); });
 
-      var enc = smart.patient.api.fetchAll({
-        type: "Encounter",
-      });
+//       var enc = smart.patient.api.fetchAll({
+//         type: "Encounter",
+//       });
 
-      // here, we are getting here first index of encounter Details array.
+//       // here, we are getting here first index of encounter Details array.
 
-      enc.done(function (data) { sessionStorage.setItem('encounterDetails', JSON.stringify(data[0])); });
+//       enc.done(function (data) { sessionStorage.setItem('encounterDetails', JSON.stringify(data[0])); });
 
-      var prac = smart.patient.api.fetchAll({
-        type: "Practitioner",
-      });
+//       var prac = smart.patient.api.fetchAll({
+//         type: "Practitioner",
+//       });
 
-      prac.done(function (data) { sessionStorage.setItem('practitionerDetails', JSON.stringify(data)); });
+//       prac.done(function (data) { sessionStorage.setItem('practitionerDetails', JSON.stringify(data)); });
 
-        $.when(pt, obv).fail(onError);
+//         $.when(pt, obv).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
-          var byCodes = smart.byCodes(obv, 'code');
-          var gender = patient.gender;
+//         $.when(pt, obv).done(function(patient, obv) {
+//           var byCodes = smart.byCodes(obv, 'code');
+//           var gender = patient.gender;
 
-          var fname = '';
-          var lname = '';
+//           var fname = '';
+//           var lname = '';
 
-          if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-           // lname = patient.name[0].family.join(' ');
-            lname = patient.name[0].family;
-          }
+//           if (typeof patient.name[0] !== 'undefined') {
+//             fname = patient.name[0].given.join(' ');
+//            // lname = patient.name[0].family.join(' ');
+//             lname = patient.name[0].family;
+//           }
 
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
+//           var height = byCodes('8302-2');
+//           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+//           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+//           var hdl = byCodes('2085-9');
+//           var ldl = byCodes('2089-1');
 
-          var p = defaultPatient();
-          p.birthdate = patient.birthDate;
-          p.gender = gender;
-          p.fname = fname;
-          p.lname = lname;
-          p.height = getQuantityValueAndUnit(height[0]);
+//           var p = defaultPatient();
+//           p.birthdate = patient.birthDate;
+//           p.gender = gender;
+//           p.fname = fname;
+//           p.lname = lname;
+//           p.height = getQuantityValueAndUnit(height[0]);
 
-          if (typeof systolicbp != 'undefined')  {
-            p.systolicbp = systolicbp;
-          }
+//           if (typeof systolicbp != 'undefined')  {
+//             p.systolicbp = systolicbp;
+//           }
 
-          if (typeof diastolicbp != 'undefined') {
-            p.diastolicbp = diastolicbp;
-          }
+//           if (typeof diastolicbp != 'undefined') {
+//             p.diastolicbp = diastolicbp;
+//           }
 
-          p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
+//           p.hdl = getQuantityValueAndUnit(hdl[0]);
+//           p.ldl = getQuantityValueAndUnit(ldl[0]);
 
-          ret.resolve(p);
-        });
-      } else {
-        onError();
-      }
-    }
+//           ret.resolve(p);
+//         });
+//       } else {
+//         onError();
+//       }
+//     }
 
-    FHIR.oauth2.ready(onReady, onError);
-    return ret.promise();
+//     FHIR.oauth2.ready(onReady, onError);
+//     return ret.promise();
 
-  };
+//   };
 
-  function defaultPatient(){
-    return {
-      fname: {value: ''},
-      lname: {value: ''},
-      gender: {value: ''},
-      birthdate: {value: ''},
-      height: {value: ''},
-      systolicbp: {value: ''},
-      diastolicbp: {value: ''},
-      ldl: {value: ''},
-      hdl: {value: ''},
-    };
-  }
+//   function defaultPatient(){
+//     return {
+//       fname: {value: ''},
+//       lname: {value: ''},
+//       gender: {value: ''},
+//       birthdate: {value: ''},
+//       height: {value: ''},
+//       systolicbp: {value: ''},
+//       diastolicbp: {value: ''},
+//       ldl: {value: ''},
+//       hdl: {value: ''},
+//     };
+//   }
 
-  function getBloodPressureValue(BPObservations, typeOfPressure) {
-    var formattedBPObservations = [];
-    BPObservations.forEach(function(observation){
-      var BP = observation.component.find(function(component){
-        return component.code.coding.find(function(coding) {
-          return coding.code == typeOfPressure;
-        });
-      });
-      if (BP) {
-        observation.valueQuantity = BP.valueQuantity;
-        formattedBPObservations.push(observation);
-      }
-    });
+//   function getBloodPressureValue(BPObservations, typeOfPressure) {
+//     var formattedBPObservations = [];
+//     BPObservations.forEach(function(observation){
+//       var BP = observation.component.find(function(component){
+//         return component.code.coding.find(function(coding) {
+//           return coding.code == typeOfPressure;
+//         });
+//       });
+//       if (BP) {
+//         observation.valueQuantity = BP.valueQuantity;
+//         formattedBPObservations.push(observation);
+//       }
+//     });
 
-    return getQuantityValueAndUnit(formattedBPObservations[0]);
-  }
+//     return getQuantityValueAndUnit(formattedBPObservations[0]);
+//   }
 
-  function getQuantityValueAndUnit(ob) {
-    if (typeof ob != 'undefined' &&
-        typeof ob.valueQuantity != 'undefined' &&
-        typeof ob.valueQuantity.value != 'undefined' &&
-        typeof ob.valueQuantity.unit != 'undefined') {
-          return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
-    } else {
-      return undefined;
-    }
-  }
+//   function getQuantityValueAndUnit(ob) {
+//     if (typeof ob != 'undefined' &&
+//         typeof ob.valueQuantity != 'undefined' &&
+//         typeof ob.valueQuantity.value != 'undefined' &&
+//         typeof ob.valueQuantity.unit != 'undefined') {
+//           return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
+//     } else {
+//       return undefined;
+//     }
+//   }
 
-  window.drawVisualization = function(p) {
-    $('#holder').show();
-    $('#loading').hide();
-    $('#fname').html(p.fname);
-    $('#lname').html(p.lname);
-    $('#gender').html(p.gender);
-    $('#birthdate').html(p.birthdate);
-    $('#height').html(p.height);
-    $('#systolicbp').html(p.systolicbp);
-    $('#diastolicbp').html(p.diastolicbp);
-    $('#ldl').html(p.ldl);
-    $('#hdl').html(p.hdl);
-  };
+//   window.drawVisualization = function(p) {
+//     $('#holder').show();
+//     $('#loading').hide();
+//     $('#fname').html(p.fname);
+//     $('#lname').html(p.lname);
+//     $('#gender').html(p.gender);
+//     $('#birthdate').html(p.birthdate);
+//     $('#height').html(p.height);
+//     $('#systolicbp').html(p.systolicbp);
+//     $('#diastolicbp').html(p.diastolicbp);
+//     $('#ldl').html(p.ldl);
+//     $('#hdl').html(p.hdl);
+//   };
 
-})(window);
+// })(window);
 /* its working for smart launcher end*/
 
 
